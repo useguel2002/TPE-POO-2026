53c1862da0057159463292a0513e743539eae6e3
Ulises Seguel <useguel2002@gmail.com>
2026-02-17 14:38:27 -0300

    JavaFX (Ya muestra)

--- src/main/java/org/example/tpe_poo_ulises_seguel/HelloController.java
+++ src/main/java/org/example/tpe_poo_ulises_seguel/HelloController.java
@@ -21,6 +21,7 @@ public class HelloController {
     public void initialize() {
         try {
             tablero = TableroCarga.desdeArchivo("src/main/resources/Ejemplos/Ejemplo 2.txt");
+            dibujar();
         } catch (IOException e) {
             System.out.println("Error al cargar archivo: " + e.getMessage());
             return; //en caso que Tablero siga NULL volver/terminar
@@ -31,6 +32,11 @@ public class HelloController {
             for (int col = 0; col < tablero.getColumnas(); col++) {
                 Pane celda = new Pane();
                 celda.setStyle("-fx-border-color: black;");
+                if (tablero.getCelda(fila, col).estaViva()) {
+                    celda.setStyle("-fx-background-color: black;");
+                } else {
+                    celda.setStyle("-fx-background-color: white;");
+                }
                 celda.setPrefSize(20, 20);
                 grilla.add(celda, col, fila);
             }
